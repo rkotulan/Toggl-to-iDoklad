a29cbaeed0bdc2f3994630115927a7aa6d1312f6
Rudolf Kotulán <rudolf@kotulan.eu>
2016-10-23 22:07:39 +0200

    Přidání souboru company info sdružující informace z AssemblyInfo

---
+++ Source/TogglToInvoice.Common/Shared/CompanyInfo.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CompanyInfo.cs" company="Rudolf Kotulán">
+//   Copyright © Rudolf Kotulán All Rights Reserved
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace TogglToInvoice.Common.Shared
+{
+    /// <summary>
+    ///     Global information about assembly version, company and product info.
+    /// </summary>
+    public static class CompanyInfo
+    {
+        /// <summary>
+        ///     Company name
+        /// </summary>
+        public const string CompanyName = "Rudolf Kotulán";
+
+        /// <summary>
+        ///     Copyright information
+        /// </summary>
+        public const string Copyright = "Copyright © Rudolf Kotulán";
+
+        /// <summary>
+        ///     File version info
+        /// </summary>
+        public const string FileVersion = Version;
+
+        public const string Title = "Toggl to iDoklad";
+
+        /// <summary>
+        ///     Assembly version
+        /// </summary>
+        public const string Version = "1.0.0.0";
+    }
+}
--- Source/TogglToInvoice.WinApp/Properties/AssemblyInfo.cs
+++ Source/TogglToInvoice.WinApp/Properties/AssemblyInfo.cs
@@ -1,16 +1,23 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Rudolf Kotulán">
+//   Copyright © Rudolf Kotulán All Rights Reserved
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
 using System.Runtime.InteropServices;
+
+using TogglToInvoice.Common.Shared;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TogglToInvoice.WinApp")]
+[assembly: AssemblyTitle(CompanyInfo.Title)]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TogglToInvoice.WinApp")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct(CompanyInfo.Title)]
+[assembly: AssemblyCopyright(CompanyInfo.Copyright)]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -23,14 +30,12 @@ using System.Runtime.InteropServices;
 [assembly: Guid("153b4eff-1857-4714-b027-c74ec583893f")]
 
 // Version information for an assembly consists of the following four values:
-//
 //      Major Version
 //      Minor Version 
 //      Build Number
 //      Revision
-//
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion(CompanyInfo.Version)]
+[assembly: AssemblyFileVersion(CompanyInfo.FileVersion)]
